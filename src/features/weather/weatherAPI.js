16c98233a1bbe4e78b8f235b1dd14cb9d4a78c53
Ky3bK4 <k.kyzmin321@gmail.com>
2021-11-16 14:34:40 +0200

    add https protocol

--- src/features/weather/weatherAPI.js
+++ src/features/weather/weatherAPI.js
@@ -2,7 +2,7 @@ import { checkStatus } from "../../utils";
 
 export const fetchDaily = async (city) => {
   return await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=d6c2f9bbc1b6409ea6872659210911&q=${city}&aqi=no`
+    `https://api.weatherapi.com/v1/current.json?key=d6c2f9bbc1b6409ea6872659210911&q=${city}&aqi=no`
   )
     .then(checkStatus)
     .then((res) => res.json())
@@ -11,7 +11,7 @@ export const fetchDaily = async (city) => {
 
 export const fetchHourly = async (city) => {
   return await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=d6c2f9bbc1b6409ea6872659210911&q=${city}&days=8&aqi=no&alerts=no`
+    `https://api.weatherapi.com/v1/forecast.json?key=d6c2f9bbc1b6409ea6872659210911&q=${city}&days=8&aqi=no&alerts=no`
   )
     .then(checkStatus)
     .then((response) => response.json())
@@ -20,7 +20,7 @@ export const fetchHourly = async (city) => {
 
 export const fetchSearch = async (query) => {
   return await fetch(
-    `http://api.weatherapi.com/v1/search.json?key=d6c2f9bbc1b6409ea6872659210911&q=${query}`
+    `https://api.weatherapi.com/v1/search.json?key=d6c2f9bbc1b6409ea6872659210911&q=${query}`
   )
     .then(checkStatus)
     .then((res) => res.json())
